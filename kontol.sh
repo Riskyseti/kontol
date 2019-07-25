67ceabf4edcaa4ed2c6ee75faa7953a981df1cbb
Riskyseti <53308985+Riskyseti@users.noreply.github.com>
2019-07-25 22:45:10 +0700

    Add files via upload

---
+++ kontol.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+### Color ###
+{
+white='\e[1;37m'
+red='\e[1;31m'
+blue='\e[0;34m'
+lightgreen='\e[1;32m'
+green='\e[32;1m'
+}
+
+
+echo -e $red"×××××××××××××××××××××××××××××××××××××××××××××××××××××××××××××"
+echo -e $green "Mitsuki Tersakiti"
+echo -e $red  " ___  ____ ____ ____ ____ ____             "$red     "*****" 
+echo -e $red    " |  \ |___ |___ |__| |    |___             "$green "********"
+echo -e $white " |__/ |___ |    |  | |___ |___             "$white  "***********"
+
+echo -e $blue"                            Night people Team"
+echo -e $red "×××××××××××××××××××××××××××××××××××××××××××××××××××××××××××××"
+echo -e $white "Author : Risky Setiawan"  |lolcat
+echo -e $blue  "Contak : 085200070386"    |lolcat
+echo -e $blue "============================================================="
+echo -e $red   "==================================================================="
+echo -e $red   "($).Masukan web tujuan anda"
+echo -e $white "($).ganti scrip deface kamu menjadi index.html"
+echo -e $green "($).salin Targed web nya dan tempel di bawah dengang index.html"
+echo -e $white "====================================================================="
+echo -e $blue "(#)salin web target"
+read bil1
+echo -e $blue "(#)ketik (y/n) untuk proses pengambilan link"
+read bi2
+echo -e $green"(#) bisa di salin link nya: https://xnxx.coms.live"
+echo -e $blue "_______________________________________________________________________"
+echo -e $blue "............... Trimakasih Telah Mampir Di Tolls Saya.................." |lolcat
+
+
+
+
